a2b39473710d7a23b086d7f020375b6ab567715d
Ibrahim Khan <aitvetibrahim@gmail.com>
2021-10-20 00:50:22 +0600

    create auth context hook

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -16,21 +16,26 @@ Step-1: Initial Setup
 4.initialize firebase
 5.Enable auth method
 ---------------------
-Step-2:
+Step-2: setup component
 1. Create Login Component
 2. Create Register Component
 3. Create Route for Login and Register
 --------------------------------------
-Step-3:
+Step-3: set auth system
 1.setup sing in method
 2.setup sing out method
 3.user state
 4.sepcial observer
 5.return necessary methods and states from useFirebase
 ------------------------------
-Step-4:
+Step-4: create auth context hook (useAuth)
 1.create a auth context
 2.create context provider
-3.use auth provider
-4.create useAuth hook
+3.set context provider context value
+4.use auth provider
+5.create useAuth hook
+-------------------------
+Step-5: create private route
+1.create private Route
+2.set private route
 */
